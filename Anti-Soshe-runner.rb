1c7fcea2674ad14aa43be866e2d3325f349f4a9c
Joe Burdick <josephdburdick@gmail.com>
2013-11-18 00:47:02 -0500

    Added private helper function, minimizing code.

--- Anti-Soshe-runner.rb
+++ Anti-Soshe-runner.rb
@@ -1,16 +1,18 @@
 class Call
-	attr_accessor :name
-	def initialize name="Stranger"
-		@name = name
-
-		@account_sid = ENV["TWILIO_SID"]
-		@auth_token = ENV["TWILIO_TOKEN"]
-		@client = Twilio::REST::Client.new(@account_sid, @auth_token)
-		@account = @client.account
-		introduce
+	ACCOUNT_SID = ENV["TWILIO_SID"]
+	AUTH_TOKEN = ENV["TWILIO_TOKEN"]
+	
+	def initialize
+		@account = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
 	end
 
-	def makeCall
+  def run
+    introduce
+    ask_minutes
+    ask
+  end
+
+	def make_call
 		call = @account.calls.create({
 			:url => "http://joeylabs.com/projects/twilio/voice.xml",
 			:from => '+13473345606', 
@@ -21,28 +23,25 @@ class Call
 	
 	def introduce
 		puts "\nHello #{name}! Welcome to ANTI-SOSHE, your social eject button."
-		askMinutes
+		ask_minutes
 	end
-	def askMinutes
+	def ask_minutes
 		puts "How many minutes from now would you like us to call you so you can excuse yourself?"
-		@minutes = gets.chomp.to_i
+		@minutes = gets.to_i
 		ask
 	end
-	def confirmMinutes
+	def confirm_minutes
 		confirm = gets.chomp.downcase
-
 			if yesArray.include?(confirm)
-
-					if @minutes == 1
-						
-						timeCondition = "a minute"
-					else
-						timeCondition = "#{@minutes} minutes."
-					end
-					puts "\nOkay, great! We'll schedule your call to occur #{timeCondition}"
-					queue
+				if @minutes == 1
+					timeCondition = "a minute"
+				else
+					timeCondition = "#{@minutes} minutes."
+				end
+				puts "\nOkay, great! We'll schedule your call to occur #{timeCondition}"
+				queue
 			elsif noArray.include?(confirm)
-				askMinutes
+				ask_minutes
 			else 
 				puts "\n\nWe did not understand your command. Goodbye.\n"
 			end
@@ -58,17 +57,22 @@ class Call
 		@noArray = ["false", "f", "no", "naw", "nope", "na", "nah", "n"]
 	end
 	def ask
-		if @minutes == 0
-			puts "\n\nSorry, we need at least a minute to schedule your call."
-			askMinutes
-		elsif @minutes == 1
-			puts "\n\nSchedule the call in #{@minutes} minute?"
-			confirmMinutes
-		else
-			puts "\n\nSchedule the call in #{@minutes} minutes?"
-			confirmMinutes
-		end
+    if @minutes.zero?
+      return puts "\n\nSorry, we need at least a minute to schedule your call."
+    end
+    puts "\n\nSchedule the call in #{@minutes} #{pluralize(@minutes, 'minute')}?"
+    confirm_minutes
 	end
+
+  private
+
+  def pluralize(count, word)
+    if count != 1
+      word + 's'
+    else
+      word
+    end
+  end
 end
 
 
